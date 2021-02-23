b861963a5129dcd48148b4facaf5b7b100e84c1e
mac <mac@macs-iMac.local>
2021-02-23 18:46:06 +0530

    change path

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AuthModule } from '../../src/auth/auth.module';
-import { AuthService } from '../../src/auth/auth.service';
-import { userSchema } from '../../src/auth/Schema/users.schema';
+import { AuthModule } from '../auth/auth.module';
+import { AuthService } from '../auth/auth.service';
+import { userSchema } from '../auth/Schema/users.schema';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
