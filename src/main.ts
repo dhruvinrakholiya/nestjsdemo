1e76554ebac8775454e1f13caefd3a2bf119d9f4
mac <mac@macs-iMac.local>
2021-02-24 10:17:21 +0530

    add consolelog

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create < NestExpressApplication>(AppModule);
   await app.listen(process.env.PORT || 3000,function(){
-  console.log("Express server listening on port %d in mode", this.address().port);
+    console.log('process.env.PORT :>> ', process.env.PORT);
+    console.log("Express server listening on port %d in mode", this.address().port);
 });
+
 }
 bootstrap();
