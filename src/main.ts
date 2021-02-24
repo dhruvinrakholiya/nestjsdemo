4a60c704fdab69996c2129b6832608c0ce1c6772
mac <mac@macs-iMac.local>
2021-02-24 18:29:47 +0530

    add enablecore

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create < NestExpressApplication>(AppModule);
+  app.enableCors();
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
   await app.listen(process.env.PORT || 3000,function(){
     console.log('process.env.PORT :>> ', process.env.PORT);
     console.log("Express server listening on port %d in mode", this.address().port);
