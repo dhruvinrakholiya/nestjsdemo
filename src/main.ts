151cfa0eb6b89d5e49b84d8506a45d086f8b168e
mac <mac@macs-iMac.local>
2021-04-02 15:50:18 +0530

    change core setting

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,23 @@ const cloudinary = require('cloudinary').v2;
 config();
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.enableCors();
+  app.enableCors({origin:true});
+
   app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
+    try {
+      res.removeHeader('x-powered-by');
+      res.header('Access-Control-Allow-Credentials', 'true');
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Methods", "OPTIONS, GET,PUT,POST,DELETE");
+      res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, X-Requested-With, Authorization");
+      next();
+    } catch (e) {
+      console.log('\n err : ', e);
+    }
+    // res.header('Access-Control-Allow-Origin', '*');
+    // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    // res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    // next();
   });
   console.log('process.env.PORT :>> ', process.env.PORT);
   await app.listen(process.env.PORT || 3000, function () {
